9b86ab754b838c1a0e1fcc61924ae020aa3851d5
kiru1029 <kcanamaque1997@gmail.com>
2023-04-17 01:50:52 +0800

    Build the Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ const App = () => {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
